8fbc06f5a4bf33f6ada47113c54614b638e8b458
yaok <yaok@pingansec.com>
2021-03-30 22:49:03 +0800

    first commit

---
+++ 01-复杂度/src/com/mj/Main.java
@@ -0,0 +1,48 @@
+package com.mj;
+
+public class Main {
+	
+	/*
+	 * 0 1 1 2 3 5 8 13....
+	 */
+	
+	/**
+	 * 普通实现
+	 * @param n
+	 * @return
+	 */
+	public static int fib(int n) {
+		if (n <=1) return n;
+		
+		int i = 0;
+		int j = 1;
+		while(n>1) {
+			int tmp = i + j;
+			i = j;
+			j = tmp;
+			n--;
+		}
+		return j;
+	}
+	
+	/**
+	 * 递归实现
+	 * @param n
+	 * @return
+	 */
+	public static int fib2(int n) {
+		if (n <=1) return n;
+		return fib2(n-1) + fib2(n-2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println(123);
+		
+		System.out.println(fib(45));
+		System.out.println(fib2(45));
+		
+		
+	}
+
+}
