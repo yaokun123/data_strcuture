3497842553c4b50746b9f61ac38d8732404d57da
yaokun <yaok@pingansec.com>
2021-04-02 13:25:05 +0800

    移除链表元素

--- 00-leetcode/src/链表/_203_移除链表元素.java
+++ 00-leetcode/src/链表/_203_移除链表元素.java
@@ -6,5 +6,34 @@ package 链表;
  *
  */
 public class _203_移除链表元素 {
+	
+	public class ListNode {
+		int val;
+		ListNode next;
+		ListNode() {}
+		ListNode(int val) { this.val = val; }
+		ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
 
+	public ListNode removeElements(ListNode head, int val) {
+		ListNode current = head;
+		ListNode preNode = head;
+		while(current != null && current.next != null) {
+			if(head.val == val) {//如果是头节点要单独处理
+				head = head.next;
+				current = head;
+				preNode = head;
+			}else {
+				if(current.val == val) {
+					preNode.next = current.next;
+					current = preNode.next;
+				}else {
+					preNode = current;
+					current = current.next;
+				}
+				
+			}	
+		}
+		return head;
+    }
 }
