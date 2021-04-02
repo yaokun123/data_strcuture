a6245ff9c9829f88105363ea22f5bac8716ab385
yaokun <yaok@pingansec.com>
2021-04-02 13:36:24 +0800

    _83_删除排序链表中的重复元素

--- 00-leetcode/src/链表/_83_删除排序链表中的重复元素.java
+++ 00-leetcode/src/链表/_83_删除排序链表中的重复元素.java
@@ -7,4 +7,31 @@ package 链表;
  */
 public class _83_删除排序链表中的重复元素 {
 
+	public class ListNode {
+		 int val;
+		 ListNode next;
+		 ListNode() {}
+		 ListNode(int val) { this.val = val; }
+		 ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+	
+	
+	public ListNode deleteDuplicates(ListNode head) {
+		if (head == null || head.next == null) return head;
+		ListNode curNode = head.next;
+		ListNode preNode = head;
+		
+		while(curNode != null) {
+			if(curNode.val == preNode.val) {
+				//删除当前
+				preNode.next = curNode.next;
+				curNode = preNode.next;
+			}else {
+				preNode = curNode;
+				curNode = curNode.next;
+			}
+		}
+		
+		return head;
+    }
 }
