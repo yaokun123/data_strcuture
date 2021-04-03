0958f1e13e80045e02e6f3e742e71f18a0e28c37
yaok <yaok@pingansec.com>
2021-04-03 13:13:51 +0800

    添加虚拟头节点的链表

--- 03-链表/src/com/mj/Main.java
+++ 03-链表/src/com/mj/Main.java
@@ -17,6 +17,21 @@ public class Main {
 		
 		list.remove(1);
 		System.out.println(list);
+		
+		System.out.println("============使用虚拟头节点的链表============");
+		
+		
+		LinkList2<Object> list2 = new LinkList2<>();
+		list2.add(1);
+		list2.add("test1");
+		list2.add(121);
+		list2.add(1111);
+		list2.add(0,"hahaha");
+		
+		System.out.println(list2);
+		
+		list2.remove(1);
+		System.out.println(list2);
 	}
 
 }
