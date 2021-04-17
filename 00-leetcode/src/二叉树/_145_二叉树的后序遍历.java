a03207d0619eb135a008e0f7916f8d4ac2293bcb
yaok <yaok@pingansec.com>
2021-04-17 22:06:23 +0800

    二叉树后序遍历

---
+++ 00-leetcode/src/二叉树/_145_二叉树的后序遍历.java
@@ -0,0 +1,22 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _145_二叉树的后序遍历 {
+	public List<Integer> postorderTraversal(TreeNode root) {
+		List<Integer> res = new ArrayList<Integer>();
+		
+		postorder(root,res);
+		return res;
+    }
+	
+	public void postorder(TreeNode node,List<Integer> l) {
+		if (node == null) return;
+		
+		postorder(node.left,l);
+		postorder(node.right,l);
+		l.add(node.val);
+		
+	}
+}
