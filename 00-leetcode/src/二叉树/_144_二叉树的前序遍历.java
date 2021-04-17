502065f7ae617c5e7a92cca0adc50fb1735cf27f
yaok <yaok@pingansec.com>
2021-04-17 22:00:25 +0800

    二叉树前序遍历

---
+++ 00-leetcode/src/二叉树/_144_二叉树的前序遍历.java
@@ -0,0 +1,27 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _144_二叉树的前序遍历 {
+	
+	
+	//递归实现
+	public List<Integer> preorderTraversal(TreeNode root) {
+		
+		
+		List<Integer> res = new ArrayList<Integer>();
+		
+		preorder(root,res);
+		return res;
+    }
+	
+	public void preorder(TreeNode node,List<Integer> l) {
+		if (node == null) return;
+		
+		l.add(node.val);
+		preorder(node.left,l);
+		preorder(node.right,l);
+		
+	}
+}
