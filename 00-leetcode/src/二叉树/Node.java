92fe74b1836a882c8fd38783c296e6ef49ed711f
yaok <yaok@pingansec.com>
2021-04-18 18:59:18 +0800

    N叉树的前序遍历

--- 00-leetcode/src/二叉树/Node.java
+++ 00-leetcode/src/二叉树/Node.java
@@ -1,5 +1,24 @@
 package 二叉树;
 
-public class Node {
+import java.util.List;
 
-}
+/**
+ * https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/
+ * @author yaokun
+ *
+ */
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
