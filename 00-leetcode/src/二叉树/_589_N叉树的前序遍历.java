ba6165ba76ef8fb078b82af4f0f0c86288cb810b
yaok <yaok@pingansec.com>
2021-04-18 18:58:43 +0800

    N叉树的前序遍历

---
+++ 00-leetcode/src/二叉树/_589_N叉树的前序遍历.java
@@ -0,0 +1,5 @@
+package 二叉树;
+
+public class _589_N叉树的前序遍历 {
+
+}
---
+++ 00-leetcode/src/二叉树/_662_二叉树最大宽度.java
@@ -0,0 +1,17 @@
+package 二叉树;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * https://leetcode-cn.com/problems/maximum-width-of-binary-tree/
+ * @author yaokun
+ *
+ */
+public class _662_二叉树最大宽度 {
+	
+	public int widthOfBinaryTree(TreeNode root) {
+		return 0;
+    }
+}
