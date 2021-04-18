96532c1c5c6043d73d8c9745ce1f57745c69c2f8
yaok <yaok@pingansec.com>
2021-04-18 23:05:38 +0800

    移除链表元素

--- 00-leetcode/src/链表/_83_删除排序链表中的重复元素.java
+++ 00-leetcode/src/链表/_83_删除排序链表中的重复元素.java
@@ -34,4 +34,19 @@ public class _83_删除排序链表中的重复元素 {
 		
 		return head;
     }
+	
+	
+	public ListNode deleteDuplicates2(ListNode head) {
+        ListNode tmp = head;
+
+        while(tmp != null && tmp.next != null){
+            if(tmp.val == tmp.next.val){
+                tmp.next = tmp.next.next;
+            }else{
+                tmp = tmp.next;
+            }
+            
+        }
+        return head;
+    }
 }
