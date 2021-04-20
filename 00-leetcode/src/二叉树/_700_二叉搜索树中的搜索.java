bc011f7d112b48cf00d0e6bff67bb989fa2320e9
yaokun <yaok@pingansec.com>
2021-04-20 20:19:35 +0800

    二叉搜索树中的搜索

---
+++ 00-leetcode/src/二叉树/_700_二叉搜索树中的搜索.java
@@ -0,0 +1,24 @@
+package 二叉树;
+
+/**
+ * https://leetcode-cn.com/problems/search-in-a-binary-search-tree/
+ * @author yaok
+ *
+ */
+public class _700_二叉搜索树中的搜索 {
+	public TreeNode searchBST(TreeNode root, int val) {
+		if(root == null) return null;
+		
+		TreeNode tmpNode = root;
+		while (tmpNode != null) {
+			if(tmpNode.val == val) {
+				return tmpNode;
+			}else if(tmpNode.val > val) {
+				tmpNode = tmpNode.left;
+			}else {
+				tmpNode = tmpNode.right;
+			}
+		}
+		return null;
+    }
+}
