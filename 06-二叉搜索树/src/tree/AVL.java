9c4fbe1d72e3cffec000faf90b87058680f4c06f
yaokun <yaok@pingansec.com>
2021-04-22 13:38:11 +0800

    AVL RBT

---
+++ 06-二叉搜索树/src/tree/AVL.java
@@ -0,0 +1,14 @@
+package tree;
+
+
+//AVL树 继承自 BST
+//windows NT内核中广泛使用
+public class AVL<E> extends BST<E> {
+	
+	
+	/**
+	 * 添加导致的失衡
+	 * 最坏情况：可能会导致所有的祖先节点都失衡
+	 * 父节点，非祖先节点都不可能失衡
+	 */
+}
---
+++ 06-二叉搜索树/src/tree/RBT.java
@@ -0,0 +1,11 @@
+package tree;
+
+
+//红黑树 继承自 BST
+//1、C++ STL（比如map、set）
+//2、java的TreeMap、TreeSet、HashMap、HashSet
+//3、Linux的进程调度
+//4、Nginx的timer管理
+public class RBT<E> extends BST<E> {
+
+}
